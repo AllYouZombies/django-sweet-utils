148ec76cd2ec33d0b7da14543da4a1bd1536c6f8
Rustam Astafeev <astafeev0308@gmail.com>
2023-01-26 15:22:20 +0500

    Added pretty ChoiceField

---
+++ django_sweet_utils/serializers/fields.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+
+class ChoiceField(serializers.ChoiceField):
+
+    def to_representation(self, obj):
+        if obj == '' and self.allow_blank:
+            return obj
+        return {'value': obj,
+                'display_name': self._choices[obj]}
