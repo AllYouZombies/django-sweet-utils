c3d048ecd0abf789018199030e8d6f1bcda631ef
Rustam Astafeev <astafeev0308@gmail.com>
2023-01-26 15:38:37 +0500

    smol fix

--- django_sweet_utils/serializers/fields.py
+++ django_sweet_utils/serializers/fields.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 class ChoiceField(serializers.ChoiceField):
 
     def to_representation(self, obj):
-        if obj == '' and self.allow_blank:
-            return obj
-        return {'value': obj,
-                'display_name': self._choices[obj]}
+        if obj:
+            return {'value': obj, 'display_name': self._choices[obj]}
+        return obj
